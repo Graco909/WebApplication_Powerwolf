f5220973963de0433dbea29ca956d09efe337c34
dawid baor <dawido20012@hotmail.com>
2023-05-13 21:07:29 +0200

    otwieranie plików json i testy

---
+++ Pages/test.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using JsonFiles;
+
+namespace WebApplication_Powerwolf.Pages
+{
+    public class testModel : PageModel
+    {
+        static readonly HttpClient client = new HttpClient();
+        public void OnGet()
+        {
+            var task = client.GetAsync("http://localhost/jsons/News.json");
+            HttpResponseMessage result = task.Result;
+            List<News> news = new List<News>();
+            if (result.IsSuccessStatusCode) 
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                news = News.FromJson(jsonString);
+            }
+            ViewData["News"] = news;
+        }
+    }
+}
