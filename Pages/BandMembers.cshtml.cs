baa92feb00e2142021039f025ecf6ed9dfd87791
dawid baor <dawido20012@gmail.com>
2023-05-14 20:43:39 +0200

    czesc iformacji o zespole

--- Pages/BandMembers.cshtml.cs
+++ Pages/BandMembers.cshtml.cs
@@ -4,30 +4,20 @@ using JsonFiles;
 
 namespace WebApplication_Powerwolf.Pages
 {
-    public class testModel : PageModel
+    public class BandMembersModel : PageModel
     {
         readonly HttpClient client = new HttpClient();
         public void OnGet()
         {
-            var task = client.GetAsync("http://localhost/jsons/News.json");
-            var task2 = client.GetAsync("http://localhost/jsons/BandMembers.json");
+            var task = client.GetAsync("http://localhost/jsons/BandMembers.json");
             HttpResponseMessage result = task.Result;
-            HttpResponseMessage result2 = task2.Result;
-            List<News> news = new List<News>();
             List<BandMembers> bandmembers = new List<BandMembers>();
-            if (result.IsSuccessStatusCode) 
+            if (result.IsSuccessStatusCode)
             {
                 Task<string> readString = result.Content.ReadAsStringAsync();
                 string jsonString = readString.Result;
-                news = News.FromJson(jsonString);
-            }
-            if (result2.IsSuccessStatusCode)
-            {
-                Task<string> readString = result2.Content.ReadAsStringAsync();
-                string jsonString = readString.Result;
                 bandmembers = BandMembers.FromJson(jsonString);
             }
-            ViewData["News"] = news;
             ViewData["BandMembers"] = bandmembers;
         }
     }
