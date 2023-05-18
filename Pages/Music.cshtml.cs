0d73d56f9914ab193457bfe554af91a06a201045
Tomasz Baran <t.baran@student.po.edu.pl>
2023-05-18 19:41:36 +0200

    Dodane podstrone music, naprawiono wyświetlanie enterów, dodano sekcje popular songs.

---
+++ Pages/Music.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using JsonFiles;
+namespace WebApplication_Powerwolf.Pages;
+
+public class MusicModel : PageModel
+{
+    readonly HttpClient client = new HttpClient();
+    public void OnGet()
+    {
+        var task = client.GetAsync("http://localhost/jsons/Songs.json");
+        HttpResponseMessage result = task.Result;
+        List<Songs> songs = new List<Songs>();
+        
+        if (result.IsSuccessStatusCode)
+        {
+            Task<string> readString = result.Content.ReadAsStringAsync();
+            string jsonString = readString.Result;
+            songs = Songs.FromJson(jsonString);
+        }
+
+        List<string> albums = new List<string>();
+
+        foreach (Songs song in songs)
+        {
+            if (!albums.Contains(song.Album))
+            {
+                albums.Add(song.Album);
+            }
+        }
+
+            
+        ViewData["Songs"] = songs;
+        ViewData["Albums"] = albums;
+    }
+}
