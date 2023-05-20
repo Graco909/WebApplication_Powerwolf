14295cf534ecffabb1da67f583e2800324dc98be
Tomasz Baran <t.baran@student.po.edu.pl>
2023-05-20 18:59:44 +0200

    dodano podstrone do pojedynczej piosenki 2

---
+++ Pages/Song.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using JsonFiles;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_Powerwolf.Pages;
+
+public class Song : PageModel
+{
+    readonly HttpClient client = new HttpClient();
+
+    public Songs SelectedSong { get; set; }
+    
+    public void OnGet(string songId)
+    {
+        var task = client.GetAsync("http://localhost/jsons/Songs.json");
+        HttpResponseMessage result = task.Result;
+        List<Songs> songs = new List<Songs>();
+        
+        if (result.IsSuccessStatusCode)
+        {
+            Task<string> readString = result.Content.ReadAsStringAsync();
+            string jsonString = readString.Result;
+            songs = Songs.FromJson(jsonString);
+        }
+
+        SelectedSong = songs.First(x => x.Id == songId);
+        
+        ViewData["Song"] = SelectedSong;
+    }
+}
