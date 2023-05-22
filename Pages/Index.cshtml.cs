f8c4705889bdd004fd3a400286b194c103ac3f86
Tomasz Baran <t.baran@student.po.edu.pl>
2023-05-22 17:26:29 +0200

    jA nie wiem

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -16,36 +16,20 @@ namespace WebApplication_Powerwolf.Pages
         readonly HttpClient client = new HttpClient();
         public void OnGet()
         {
-            var task = client.GetAsync("http://localhost/jsons/News.json");
-            var task2 = client.GetAsync("http://localhost/jsons/BandMembers.json");
-            var task3 = client.GetAsync("http://localhost/jsons/Concerts.json");
-            HttpResponseMessage result = task.Result;
-            HttpResponseMessage result2 = task2.Result;
-            HttpResponseMessage result3 = task3.Result;
-            List<News> news = new List<News>();
-            List<BandMembers> bandmembers = new List<BandMembers>();
-            List<Concerts> concerts = new List<Concerts>();
-            if (result.IsSuccessStatusCode)
-            {
-                Task<string> readString = result.Content.ReadAsStringAsync();
-                string jsonString = readString.Result;
-                news = News.FromJson(jsonString);
-            }
-            if (result2.IsSuccessStatusCode)
-            {
-                Task<string> readString = result2.Content.ReadAsStringAsync();
-                string jsonString = readString.Result;
-                bandmembers = BandMembers.FromJson(jsonString);
-            }
-            if (result3.IsSuccessStatusCode)
-            {
-                Task<string> readString = result3.Content.ReadAsStringAsync();
-                string jsonString = readString.Result;
-                concerts = Concerts.FromJson(jsonString);
-            }
+            List<News> news = DataAccess.GetNews();
+            List<BandMembers> bandMembers = DataAccess.GetBandMembers();
+            List<Concerts> concerts = DataAccess.GetConcerts();
+            List<Songs> songs = DataAccess.GetSongs();
+
+            Random random = new Random();
+            
+            int index = random.Next(songs.Count);
+            Songs randomSong = songs[index];
+            
             ViewData["News"] = news;
-            ViewData["BandMembers"] = bandmembers;
+            ViewData["BandMembers"] = bandMembers;
             ViewData["Concerts"] = concerts;
+            ViewData["Song"] = randomSong;
         }
     }
 }
